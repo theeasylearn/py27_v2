2583e8027b53587972cbfdd4cbd2e035f9486371
Dpatelit <dpatel99.it@gmail.com>
2025-07-21 16:50:54 +0530

    variable

---
+++ 1_variable.py
@@ -0,0 +1,8 @@
+batch = 'python' # string data
+batch_code =27 # int data
+c= 7000.50 # float data
+on = True  # bool data
+
+print("our batch is",batch)
+print("our batch is",batch,'batch code is',batch_code,'fees are',c,'currently ongoing',on)
+print(f'our batch is {batch} batch code is {batch_code} fees are {c} currently ongoing {on}')
